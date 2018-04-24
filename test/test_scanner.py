04dac6d3d4339751baafa3bacb74e33ec9bde463
jcjimenez <jc.jimenez@microsoft.com>
2018-04-24 11:00:20 -0500

    Renamed pedestrian.py to scanner.py

--- test/test_scanner.py
+++ test/test_scanner.py
@@ -3,12 +3,10 @@ import os
 import sys
 import unittest
 
-from mock import MagicMock
-
 if __name__ == "__main__":
     sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
-class PixelatorTest(unittest.TestCase):
+class PedestrianTest(unittest.TestCase):
     def setUp(self):
         pass
     def test_hello_world(self):
